8019dc5281f49254dc1c59ded88e033251c3446e
Ssupervipuser <1343706166@qq.com>
2024-04-09 20:36:56 +0800

    配置数据库

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,28 @@
 import os, sys
-
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-
+from redis import StrictRedis
 from app.settings.config import config_dict
 from common.utils import contants
 
 # 将common文件添加到python搜索路径
-
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, BASE_PATH + '/common')
 # print(BASE_PATH)
+
+
+####################添加数据库###################
+# 方法1：db=SQLAlchemy(app)
+
+# 方法2：
+# 由于app被封装采用延后加载app数据库配置信息方法初始化数据库对象
+# 定义成全局变量，方便别的模块调用
+
+db = SQLAlchemy()
+
+# redis客户端同样需要暴露成全局变量，方便模块调用
+
+redis_cli = None  # type: StrictRedis
 
 
 def create_flask_app(type):
@@ -35,9 +48,23 @@ def create_app(type):
     :return: app
     '''
     # 1.调用内容方法生产app
-    app=create_flask_app(type)
+    app = create_flask_app(type)
     # 2.注册拓展初始化组件
-
+    register_extentions(app)
     # 3.注册蓝图初始化组件
 
     return app
+
+
+def register_extentions(app: Flask):  # 声明app形参传入的是什么
+    '''注册拓展初始化组件'''
+
+    # 1.延后加载app，进行mysql数据库对象初始化
+    db.init_app(app)
+
+    # 2.延后加载app中redis哭护短配置信息，然后创建redis客户端对象
+
+    global redis_cli
+    #decode_responses=True将返回的bytes类型转为str
+    redis_cli = StrictRedis(host=app.config['REDIS_HOST'], port=app.config['REDIS_PORT'], decode_responses=True)
+    pass
--- app/settings/config.py
+++ app/settings/config.py
@@ -1,30 +1,39 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
 class DefalutConfig():
     '''醒目默认配置信息'''
-    #session加密字符串
-    SECRET_KEY='flask_news'
+    # session加密字符串
+    SECRET_KEY = 'flask_news'
+
+    # mysql连接配置
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysql@192.168.10.131:3306/flasknews'
+    # 输出执行的sql语言
+    SQLALCHEMY_ECHO = True
+
+    # redis连接配置
+    REDIS_HOST = '192.168.10.131'
+    # 需要连接主库
+    REDIS_PORT = 6381
 
 
 class DevelopmentConfig(DefalutConfig):
     '''开发环境配置信息'''
-    DEBUG=True
-
-    #mysql连接配置
-
-    #redis连接配置
+    DEBUG = True
 
 
 class ProductionConfig(DefalutConfig):
     '''生产环境配置信息'''
 
-    DEBUG=True
+    DEBUG = True
 
     # mysql连接配置
 
     # redis连接配置
 
 
-#外界条用暴露字典接口
-config_dict={
-    'dev':DevelopmentConfig,
-    'pro':ProductionConfig,
+# 外界条用暴露字典接口
+config_dict = {
+    'dev': DevelopmentConfig,
+    'pro': ProductionConfig,
 }
